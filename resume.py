fddef4f8158b7052beba31e98970d8c49ed906dc
25DIT24F1139 <tharanist06@gmail.com>
2026-08-21 00:31:59 +0800

    Create resume.py
    Create resume.py

---
+++ resume.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Tharani Sekar | Resume",
+    page_icon="💻",
+    layout="wide"
+)
+
+st.title("THARANI SEKAR")
+st.subheader("IT NETWORKING STUDENT")
+
+st.write(
+    "I am a Semester 5 Diploma in Information Technology student "
+    "with a strong interest in computer networking, cybersecurity, "
+    "and modern technologies. I enjoy learning new technical skills, "
+    "solving problems, and gaining practical experience in the IT field."
+)
+
+st.write("📞 012-853 2854")
+st.write("📧 tharanist06@gmail.com")
+st.write("📍 Johor, Malaysia")
